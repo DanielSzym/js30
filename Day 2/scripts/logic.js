400742c45b99dbd2c152f4057d1d22afcfd0a750
DanielSzym <d.szymkowiak@gmx.net>
2018-10-02 20:13:59 +0200

    working seconds Hand

--- Day 2/scripts/logic.js
+++ Day 2/scripts/logic.js
@@ -1,3 +1,5 @@
+//TODO: Add listener to initialize when second changes
+
 //(() => { 
 
 (function(window, document, undefined){
@@ -7,19 +9,47 @@
     window.onload = initialize;
     
         function initialize(){
+
+            //listen to when second changes to initialize clock every 1/60 secs
+            var listener = getSecondDegree();
+            var listenerInterval = setInterval(function() {
+                if (listener != getSecondDegree()) {
+                    initializeSeconds();
+                    clearInterval(listenerInterval);
+                }
+            }, 16.67)
+            
+
+            function initializeSeconds(){
+                secondsDegree = getSecondDegree();
+                setSecondsHand(secondsDegree);
+            }
+        }
+
+        function setSecondsHand(secondsDegree){
+            //get Element
+            var secondsHand = document.getElementById('secondsHand');
+            //set style element
+            
+            setSecondsHandStyle(secondsHand, secondsDegree);
+            //make hand visible
+            secondsHand.style.backgroundColor = '#ffffff';
+            
+            setInterval(function() {
+                secondsDegree = getSecondDegree(); 
+                console.log(getSecondDegree());
+                secondsHand.style.transform = 'rotate('+(secondsDegree)+'deg)';
+            }, 1000);
+        }
+
+        function getSecondDegree() {
             var d = new Date();
-            seconds = d.getSeconds()
-            console.log(seconds);
-            secondsDegree = seconds*6;
-            console.log(secondsDegree);
-            
-        
-            
-            var secHand = document.getElementById('secondsHand');
-            console.log(secHand);
-            var degree = 'rotate('+secondsDegree+'deg);';
-            secHand.style.transform = degree;
-            console.log(degree);
+            return (d.getSeconds()*6)+90;
+        }
+
+        function setSecondsHandStyle(secondsHand, secondsDegree){
+            var degree = 'rotate('+secondsDegree+'deg)';
+            secondsHand.style.transform = degree;
         }
     
     })(window, document, undefined);
