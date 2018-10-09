cdfa8c6649eb6759dd077b8722591c7965b9bc9c
DanielSzym <d.szymkowiak@gmx.net>
2018-10-09 16:02:46 +0200

    get spacing & blur working

--- Day 3/scripts/logic.js
+++ Day 3/scripts/logic.js
@@ -5,24 +5,34 @@
     window.onload = initialize;
 
     function initialize() {
-        const colorPicker = document.getElementById('color');
-        const blurFilter = document.getElementById('blur');
-        
         const body = document.body;
 
+        const colorPicker = document.getElementById('color');
+        const blurFilter = document.getElementById('blur');
+        const spaceFilter = document.getElementById('spacing');
+
+        const spacingInt = document.querySelector('.spacingPx');
+        const blurInt = document.querySelector('.blurPx');
         
 
         colorPicker.addEventListener('change', watchColorPicker, false);
-        colorPicker.addEventListener('change', watchBlurFilter, false);
+        //change blur also works with mousemove but tracks every hover as well
+        blurFilter.addEventListener('change', watchBlurFilter, false);
+        spaceFilter.addEventListener('change', watchSpaceFilter, false);
 
-        function watchColorPicker() {
-            console.log('changed to ' + this.value);
+        function watchColorPicker () {
+            //console.log('changed to ' + this.value);
             body.style.setProperty('--base', this.value);
         }
 
         function watchBlurFilter (){
-            console.log(blurFilter);
-            body.style.setProperty('--blur', this.value);
+            body.style.setProperty('--blur', this.value + 'px');
+            blurInt.innerHTML = this.value;
+        }
+
+        function watchSpaceFilter (){
+            body.style.setProperty('--spacing', this.value + 'px');
+            spacingInt.innerHTML = this.value;
         }
 
         
