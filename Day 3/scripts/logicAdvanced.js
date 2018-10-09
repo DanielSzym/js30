99c30ee0cffca066c56aa7f629c07728c5621d9d
DanielSzym <d.szymkowiak@gmx.net>
2018-10-09 20:57:25 +0200

    add WesBos solution in separate js file

---
+++ Day 3/scripts/logicAdvanced.js
@@ -0,0 +1,49 @@
+(function(window, document, undefined){
+
+    // code that will be taken care of right away
+    
+    window.onload = initialize;
+
+    function initialize() {
+        const body = document.body;
+        
+        
+        const elements = document.body.querySelectorAll('.control-elements input');
+        console.log(elements);
+
+        function handleUpdate () {
+            console.log(this.update);
+        }
+
+        /*const colorPicker = document.getElementById('color');
+        const blurFilter = document.getElementById('blur');
+        const spaceFilter = document.getElementById('spacing');
+
+        const spacingInt = document.querySelector('.spacingPx');
+        const blurInt = document.querySelector('.blurPx');
+        
+
+        colorPicker.addEventListener('change', watchColorPicker, false);
+        //change blur also works with mousemove but tracks every hover as well
+        blurFilter.addEventListener('change', watchBlurFilter, false);
+        spaceFilter.addEventListener('change', watchSpaceFilter, false);
+
+        function watchColorPicker () {
+            //console.log('changed to ' + this.value);
+            body.style.setProperty('--base', this.value);
+        }
+
+        function watchBlurFilter (){
+            body.style.setProperty('--blur', this.value + 'px');
+            blurInt.innerHTML = this.value;
+        }
+
+        function watchSpaceFilter (){
+            body.style.setProperty('--spacing', this.value + 'px');
+            spacingInt.innerHTML = this.value;
+        }*/
+
+    }
+
+
+})(window, document, undefined);
